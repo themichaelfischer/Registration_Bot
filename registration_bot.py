37049af0737af522f300c7c06f5f2974d9fdb932
Michael Fischer <79065779+mfischercodes@users.noreply.github.com>
2022-09-28 21:02:34 -0700

    Working bot v1 (#2)
    * Selected Person
    
    * Working Bot v1 testing tomorrow

--- registration_bot.py
+++ registration_bot.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import Select
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
@@ -10,8 +11,13 @@ class Register():
         self._program = "volleyball"
         self._day = "Friday"
         self._info = "info.txt"
+        self._cc_info = "cc_info.txt"
         self._user = ''
         self._password = ''
+        self._card_number = ''
+        self._cvv = ''
+        self._exp_month = ''
+        self._exp_year = ''
         self._driver = self.setup_driver()
 
     # Description: set's up selenium driver
@@ -49,77 +55,88 @@ class Register():
     # Description: Gets current date to select date you wish to sign up for
     def get_date():
         # TODO get current date
-        # TODO choose corresponding date name + location
+        # TODO have corresponding date to "//div[@id='activity-1-####']"
         return "Friday"
 
     # Description: Selects the correct course
     def select_course(self):
-        
-        parent = self._driver.find_element(By.XPATH, "//div[@id='activity-1-8635']")
+        #TODO select based on current date
+        parent = self._driver.find_element(By.XPATH, "//div[@id='activity-1-8614']")
 
         #TODO make this function cleaner as multiple clicks?
         parent.find_element(By.XPATH, "./child::*").click()
-        #TODO add sleep
-        time.sleep(1)
-        # add button
-        self._driver.find_element(By.XPATH, "//a[@class='ui-state-active ui-corner-all link-button ajax-request from-full-page focus-parent need-focus-pageobject']").click()
+        time.sleep(0.5)
 
+        # Add Element is unique so just need to refresh + try this until it works
+        self._driver.find_element(By.XPATH, "//a[@class='ui-state-active ui-corner-all link-button ajax-request from-full-page focus-parent need-focus-pageobject']").click()
+        time.sleep(1)
 
     # Description: Selects the correct person to sign up
     def select_person(self):
-        pass
+        self._driver.find_element(By.XPATH, "//select[@name='ClientID']").click()
+        # TODO make this match a name instead
+        time.sleep(0.5)
+        self._driver.find_element(By.XPATH, "//option[@value='10382']").click()
         # TODO select correct person + go to credit card info
+        time.sleep(0.5)
 
-    # Description: Checks if course is available to add
-    def check_course_availability(self):
-        # change to while loop later
-        for i in range(5):
-            print(i)
-            try:
-                print("running")
-                self.select_course()
-                self.select_person()
-                print("Course Available!")
-                return True
-            except:
-                print("here")
-                time.sleep(1)
-                self._driver.refresh()
-        return False
+        self._driver.find_element(By.XPATH, "//input[@value='Go to Checkout']").click()
+
+        time.sleep(1)
 
     # Description: Pays for course at checkout page
     def pay_for_course(self):
+        self.get_card_info()
         print("Made it to sign up page")
-        pass
+
+        card_type = Select(self._driver.find_element(By.XPATH, "//select[@name='CardType']"))
+        card_type.select_by_visible_text('VISA (web)')
+
+        self._driver.find_element(By.XPATH, "//input[@name='CardNum']").send_keys(self._card_number)
+
+        self._driver.find_element(By.XPATH, "//input[@name='CardSecurityCode']").send_keys(self._cvv)
+
+        card_month = Select(self._driver.find_element(By.XPATH, "//select[@name='CardExpM']"))
+        card_month.select_by_visible_text(self._exp_month)
+        
+        card_year = Select(self._driver.find_element(By.XPATH, "//select[@name='CardExpY']"))
+        card_year.select_by_visible_text(self._exp_year)
+
+        # TODO should run once this is un commented
+        self._driver.find_element(By.XPATH, "//input[@name='ApplyPayment']").click()
+        print("Signed up!")
+
+    # Description: Get's card info from file
+    def get_card_info(self):
+        f = open(self._cc_info, "r")
+        self._card_number = f.readline().strip()
+        self._cvv = f.readline().strip()
+        self._exp_month = f.readline().strip()
+        self._exp_year = f.readline()
+
+        f.close()
 
     # Description: Destructor which quits selenium driver
     # def __del__(self):
     #     self._driver.quit()
 
 if __name__ == "__main__":
+    course_available = False
     reg = Register()
+
     reg.get_login_info()
     reg.login()
-    reg.get_page(1)
-
-    # time.sleep(1)
-    try:
-        reg.select_course()
-    except:
-        print("failed ")
-        # time.sleep(1)
-        # reg._driver.refresh()
     
-    time.sleep(2)
+    while (not course_available):
+        reg.get_page(1)
+        try:
+            reg.select_course()
+            course_available = True
+        except:
+            print("Not Yet Available")
+            time.sleep(1)
+            reg._driver.refresh()
 
-    # TODO check for waitlist condition
-    # TODO if add available some how....
-    # TODO or try to find select_course function if not continue while loop
-
-    # TODO while loop, for while add not available.. can test on thur compared to friday which has add?
-    # TODO also check when it has a waitlist, check when selecting course that title = 'ADD'
-
-    # if (reg.check_course_availability == True):
-    #     reg.pay_for_course()
-    
+    reg.select_person()
+    reg.pay_for_course()
     
